cfa0bf55ceb1070a7cfe0d6a98f09be745fe4bd0
Priya Survase <survasepriya1999@gmail.com>
2023-02-09 00:27:54 +0530

    updated Chapter 9 Assignment

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,7 +3,7 @@ const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
   //console.log(restaurant?.data?.data);
   //const { name, cuisines, cloudinaryImageId, avgRating } = restaurant;
   return (
-    <div className="w-64 m-8 border-2 p-2">
+    <div className="w-64 m-8 border-2 p-2 hover:shadow-2xl">
       <img src={`${IMG_CDN_URL}${cloudinaryImageId}`} />
       <h2 className="font-bold ">{name}</h2>
       <h3>{cuisines?.join(", ")}</h3>
